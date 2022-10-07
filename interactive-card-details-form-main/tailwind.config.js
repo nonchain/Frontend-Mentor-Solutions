89e181cf8465db6e2780abf346c10ce1959d24b7
progali007 <code.progali@gmail.com>
2022-10-07 14:22:21 +0330

    Add color palett

--- interactive-card-details-form-main/tailwind.config.js
+++ interactive-card-details-form-main/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white': 'hsl(0, 0%, 100%)',
+        'light-grayish-violet': 'hsl(270, 3%, 87%)',
+        'dark-grayish-violet': 'hsl(279, 6%, 55%)',
+        'very-dark-violet': 'hsl(278, 68%, 11%)',
+      }
+    },
   },
   plugins: [],
 }
